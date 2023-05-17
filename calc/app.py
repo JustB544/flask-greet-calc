2189191147e0054ab5e4305d85fd4c38bb73acd6
JustB544 <breytonpabst@gmail.com>
2023-05-17 15:02:42 -0400

    added functionality

--- calc/app.py
+++ calc/app.py
@@ -1,1 +1,27 @@
-# Put your app in here.
+from operations import add, sub, mult, div
+from flask import Flask, request
+
+app = Flask(__name__)
+
+operators = {
+        "add": add,
+        "sub": sub,
+        "mult": mult,
+        "div": div
+}
+
+@app.route("/<operation>")
+def do_operation(operation):
+    a = int(request.args.get("a"))
+    b = int(request.args.get("b"))
+    _return = operators[operation](a, b)
+    return str(_return)
+
+@app.route("/math/<operation>")
+def do_math(operation):
+    a = int(request.args.get("a"))
+    b = int(request.args.get("b"))
+    _return = operators[operation](a, b)
+    return str(_return)
+
+
